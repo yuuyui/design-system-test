e7a1b50952deba11dcb3c1378137d953d332a4f9
yuuyui <162280296+yuuyui@users.noreply.github.com>
2026-03-25 21:50:50 +0700

    Add initial styling to Button component

---
+++ Button.jsx
@@ -0,0 +1,9 @@
+display: inline-flex;
+padding-bottom: var(--spacing-1, 4px);
+justify-content: center;
+align-items: center;
+gap: var(--spacing-2, 8px);
+
+border-radius: var(--radius-xl, 12px);
+opacity: var(--opacity-100, 1);
+background: var(--btn-bg-btn2-connect-2, #7E57C2);
